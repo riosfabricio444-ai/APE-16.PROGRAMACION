19dae3bb0c3bd79718c5747f966639140d48cb30
riosfabricio444-ai <riosfabricio444@gmail.com>
2026-07-26 12:05:21 -0500

    Update bidimen.c

--- bidimen.c
+++ bidimen.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+// Definición de las dimensiones constantes de las matrices (2 filas x 3 columnas)
 const int FIL = 2;
 const int COL = 3;
 
+// Pide al usuario que ingrese los valores para cada matriz.
 void completarMatrices(int matriz[FIL][COL]){
 
     for (int f = 0; f < FIL; f++){ 
@@ -13,6 +15,7 @@ void completarMatrices(int matriz[FIL][COL]){
     }
 }
 
+// Suma elemento a elemento dos matrices de dimensiones 2x3.
 void sumaMatriz(int matriz1[FIL][COL], int matriz2[FIL][COL], int resultado[FIL][COL]){
     for (int f = 0; f < FIL; f++){     
         for (int c = 0; c < COL; c++){
@@ -20,6 +23,8 @@ void sumaMatriz(int matriz1[FIL][COL], int matriz2[FIL][COL], int resultado[FIL]
         }
     }
 }
+
+// Resta elemento a elemento dos matrices de dimensiones 2x3.
 void restaMatriz(int matriz1[FIL][COL], int matriz2[FIL][COL], int resultado[FIL][COL]){
     for (int f = 0; f < FIL; f++){     
         for (int c = 0; c < COL; c++){
@@ -27,6 +32,8 @@ void restaMatriz(int matriz1[FIL][COL], int matriz2[FIL][COL], int resultado[FIL
         }
     }
 }
+
+// Realiza la multiplicación elemento a elemento entre dos matrices.
 void multiplicacionMatriz(int matriz1[FIL][COL], int matriz2[FIL][COL], int resultado[FIL][COL]){
     for (int f = 0; f < FIL; f++){     
         for (int c = 0; c < COL; c++){
@@ -34,6 +41,8 @@ void multiplicacionMatriz(int matriz1[FIL][COL], int matriz2[FIL][COL], int resu
         }
     }
 }
+
+// Imprime en consola el contenido de una matriz dada, organizándolo en formato de filas y columnas con corchetes y tabulaciones.
 void mostrarResultado(int matriz[FIL][COL]){
     for (int f = 0; f < FIL; f++){     
         for (int c = 0; c < COL; c++){
@@ -47,16 +56,20 @@ int main() {
     int matrizC[FIL][COL], matrizD[FIL][COL];
     int suma[FIL][COL], resta[FIL][COL], multiplicacion[FIL][COL];
     int a, b;
+
+    //Encabezado de mi programa
     printf("--------------------------------------------");
     printf("\n OPERACIONES CON MATRICES DE 2 x 3:\n");
     printf("--------------------------------------------");
-    
+
+    // Lectura de valores por consola para matrizC y matrizD
     printf("\nMostrando los datos de la matriz C:\n");
     completarMatrices(matrizC);
 
     printf("\nMostrando los datos de la matriz D:\n");
     completarMatrices(matrizD);
 
+    // Muestra en pantalla de las matrices ingresadas
     printf("---------------------\n");
     printf("MATRIZ A:\n");
     mostrarResultado(matrizC);
@@ -66,20 +79,24 @@ int main() {
     mostrarResultado(matrizD);
     printf("---------------------\n");
 
+    // Ejecución de las operaciones matriciales
     sumaMatriz(matrizC, matrizD, suma);
     restaMatriz(matrizC, matrizD, resta);
     multiplicacionMatriz(matrizC, matrizD, multiplicacion);
-    
+
+    //Resultado de la suma
     printf("--------------------------------------------");
     printf("\nResultado de la suma:\n");
     mostrarResultado(suma);
     printf("--------------------------------------------\n");
-    
+
+    //Resultado de la resta
     printf("--------------------------------------------");
     printf("\nResultado de la resta:\n");
     mostrarResultado(resta);
     printf("--------------------------------------------\n");
-    
+
+    //Resultado de la multiplicacion
     printf("--------------------------------------------");
     printf("\nResultado de la multiplicacion:\n");
     mostrarResultado(multiplicacion);
